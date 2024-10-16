292b7b317e636bf18f7c712ead44e93953e04554
Gabriela M <g.moyamoreno@gmail.com>
2024-10-15 22:14:42 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿// WorkWithIntegers();
+/*hora que has visto los diferentes tipos numéricos, 
+escribe un código que calcule el área de un círculo cuyo radio sea de 2,50 centímetros. 
+Recuerda que el área de un círculo es el radio al cuadrado multiplicado por PI. 
+
+Una sugerencia: .NET contiene una constante para PI, Math.PI, que puede usar para ese valor. 
+Math.PI, como todas las constantes declaradas en el espacio de nombres, es un valor. 
+Por esa razón, debe usar valores en lugar de valores para este desafío.
+System .Math double doubledecimal*/
+using System;
+double radio = 2.50;
+double area = Math.PI * Math.Pow(radio, 2);
+Console.WriteLine(area);
+
+void WorkWhitDecimal(){
+
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
+
+}
+
+
+void WorkWithDoubles(){
+
+double a = 5;
+double b = 4;
+double c = 2;
+double d = (a + b) / c;
+Console.WriteLine(d);
+
+double e = 19;
+double f = 23;
+double g = 8;
+double h = (e + f) / g;
+Console.WriteLine(h);
+
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
+
+}
+
+
+void TestLimits(){
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
+}
+
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
+
+void OrderPrecedence()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+    d = (a + b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
+}
